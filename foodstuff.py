16c2ea9f621d787f9f145898597588cc84ac83ed
noopy0415 <noopy0415@gmail.com>
2018-12-03 22:01:33 +0900

    旬の食材情報取得クラス作成

---
+++ foodstuff.py
@@ -0,0 +1,52 @@
+import urllib.request, urllib.error
+from bs4 import BeautifulSoup
+
+
+class Foodstuff(object):
+    def get_foods(self, url=""):
+        url = "https://k52.org/syokuzai/" + url
+
+        html = urllib.request.urlopen(url)
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        td_ = soup.find_all('td')
+
+        foods_list = []
+
+        for tag in td_:
+            try:
+                if tag.get("class").pop(0) in "name":
+                    foods_list.append(tag.string)
+            except:
+                pass
+
+        foods_list.remove("食材名")
+
+        if None in foods_list: foods_list.remove(None)
+
+        return foods_list
+
+    def get_vegetable(self):
+        vegetable_list = Foodstuff().get_foods("vegetable/")
+        return vegetable_list
+
+    def get_fruits(self):
+        fruits_list = Foodstuff().get_foods("fruit/")
+        return fruits_list
+
+    def get_fish(self):
+        fish_list = Foodstuff().get_foods("fish/")
+        return fish_list
+
+    def get_marine(self):
+        marine_list = Foodstuff().get_foods("marine/")
+        return marine_list
+
+
+if __name__ == "__main__":
+    food = Foodstuff()
+
+    print(food.get_foods())
+
+    print(len(food.get_foods()))
