d72807105506a25b6715708cad494ee66a639227
noopy0415 <noopy0415@gmail.com>
2018-12-05 21:01:28 +0900

    返信でランダム野菜を返すように変更(お試し)

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text = "おはよう"
+    text = Foodstuff().get_food()
     line_bot_api.reply_message(event.reply_token,
                                TextSendMessage(text=text))
     # line_bot_api.reply_message(event.reply_token,
