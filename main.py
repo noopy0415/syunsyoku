b74bba02cd06e94633d558399e7b8159420c6975
noopy0415 <noopy0415@gmail.com>
2018-12-09 10:06:51 +0900

    一旦コピペ

--- main.py
+++ main.py
@@ -45,18 +45,29 @@ def handle_message(event):
     food = Foodstuff().get_food()
     recipe = Recipe().get_recipes(food).pop(0)
 
-    notes = {[CarouselColumn(thumbnail_image_url=recipe["image"],
-                             title=f"{food}のレシピ",
-                             text=recipe["recipe"],
-                             actions=[
-                                 {"type": "message", "label": "サイトURL2", "text": recipe["link"]}])],
-             [CarouselColumn(thumbnail_image_url=recipe["image"],
-                             title=f"{food}のレシピ",
-                             text=recipe["recipe"],
-                             actions=[
-                                 {"type": "message", "label": "サイトURL2", "text": recipe["link"]}])]
-             }
+    # notes = [CarouselColumn(thumbnail_image_url=recipe["image"],
+    #                         title=f"{food}のレシピ",
+    #                         text=recipe["recipe"],
+    #                         actions=[
+    #                             {"type": "message", "label": "サイトURL2", "text": recipe["link"]}])]
 
+    notes = [CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle02.jpg",
+                            title="【ReleaseNote】トークルームを実装しました。",
+                            text="creation(創作中・考え中の何かしらのモノ・コト)に関して、意見を聞けるようにトークルーム機能を追加しました。",
+                            actions=[
+                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/7"}]),
+
+             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle03.jpg",
+                            title="ReleaseNote】創作中の活動を報告する機能を追加しました。",
+                            text="創作中や考え中の時点の活動を共有できる機能を追加しました。",
+                            actions=[
+                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/6"}]),
+
+             CarouselColumn(thumbnail_image_url="https://renttle.jp/static/img/renttle04.jpg",
+                            title="【ReleaseNote】タグ機能を追加しました。",
+                            text="「イベントを作成」「記事を投稿」「本を登録」にタグ機能を追加しました。",
+                            actions=[
+                                {"type": "message", "label": "サイトURL", "text": "https://renttle.jp/notes/kota/5"}])]
     messages = TemplateSendMessage(alt_text='template',
                                    template=CarouselTemplate(columns=notes), )
     # messages = TextSendMessage(text=f'{food}のレシピ\n{recipes["link"]}')
